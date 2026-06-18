9d06c455be0585b40e9d95ec3df58a664688dfb1
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:07:30 -0300

    Update UsuarioController.java

--- src/main/java/br/com/senac/prjint3/controller/UsuarioController.java
+++ src/main/java/br/com/senac/prjint3/controller/UsuarioController.java
@@ -17,12 +17,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
 @Tag(name = "Usuários", description = "CRUD de usuários com exclusão lógica")
+@CrossOrigin("*")    
 public class UsuarioController {
 
     private final UsuarioService service;
